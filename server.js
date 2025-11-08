a3f3935bbf32c26fbe244b653cc96341a5590531
vasudhawaman <144978373+vasudhawaman@users.noreply.github.com>
2025-11-08 17:01:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static('public'));
